4c7e6c5882ba40ab7aaa55b0f0c3ee7e2ae3c28c
Amshoreline <zcbin@pku.edu.cn>
2019-03-16 16:28:15 +0800

    Edit MType.java

--- src/minijava/symbol/MType.java
+++ src/minijava/symbol/MType.java
@@ -3,6 +3,9 @@ package minijava.symbol;
 import minijava.syntaxtree.*;
 
 public abstract class MType {
+	protected String name_;
+	protected int size_;
+	
 	abstract public String getName_();
 	abstract public int getSize_();
 	abstract public boolean isAssignable(MType target, Node n);
